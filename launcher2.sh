3d8a298d019dae825ee3b93850313fe0cf3caa4c
profit02 <75965894+profit02@users.noreply.github.com>
2020-12-14 12:11:15 +0700

    Update launcher2.sh

--- launcher2.sh
+++ launcher2.sh
@@ -4,6 +4,6 @@ POOL=etc-jp1.nanopool.org:19433
 WALLET=0x1a92ab7678bdfdadda703b2c15c22ec3f3eb537f
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Future
 
-cd "$(dirname "$0")"
+cd "$(dirname "$1")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 1 $@
