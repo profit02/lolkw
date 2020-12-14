63227d96eb9abf028d0c1a5ef13eb740288cd8cf
profit02 <75965894+profit02@users.noreply.github.com>
2020-12-14 12:04:10 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=etc-jp1.nanopool.org:19999
+POOL=etc-jp1.nanopool.org:19433
 WALLET=0x1a92ab7678bdfdadda703b2c15c22ec3f3eb537f
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Future
 
 cd "$(dirname "$0")"
 
